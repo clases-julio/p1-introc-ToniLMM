242a7d0743cc61484f2d56419afa1cd21e2575b5
ToniLMM <t.mari.2021@alumnos.urjc.es>
2022-10-04 23:48:17 +0200

    avances

--- ejercicio8a.cpp
+++ ejercicio8a.cpp
@@ -4,3 +4,28 @@ Author: Toni Mari
 Date: 28/09/2022
 Goal: 
 ---------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int numero;
+int factorial;
+int i;
+
+factorial = 1;
+i = 1;
+
+cout <<"Write a non negative natural number: ";
+cin >> numero;
+
+while(i < numero){
+    i++;
+    factorial *= i; // = factorial = factorial * i
+}
+
+cout <<"The factorial of " << numero << " is " << factorial << endl;
+return 0;
+}
+
+
