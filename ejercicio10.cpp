c6aaf29bd76f6f6d258e53d64c063a16a3d6b5d0
Toni Mari Mari <t.mari.2021@alumnos.urjc.es>
2022-10-06 21:08:26 +0200

    ejercicio 10 completado

--- ejercicio10.cpp
+++ ejercicio10.cpp
@@ -4,3 +4,29 @@ Author: Toni Mari
 Date: 28/09/2022
 Goal: 
 ---------------------------------------------------*/
+#include <iostream>
+using namespace std;
+int main (){
+    int n = 0, ast = 0, esp = 0;
+    cout << "How many characters will have in the middle of the rhombus: ";
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(esp = i; esp <= n-1; esp++){
+            cout << " ";
+
+        }
+        for (ast = 1; ast <= (2*i)-1; ast++){
+            cout <<"*";
+        }
+        cout << endl;
+    }
+    for(int i = n-1; i >= 1; i--){  
+        for(esp = i; esp <= n-1; esp++){
+             cout << " ";
+        }
+        for(ast = 1; ast <= (2*i)-1; ast++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
