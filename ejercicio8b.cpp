863379858302642cad170d621d16dbc677bd3210
Toni Mari Mari <t.mari.2021@alumnos.urjc.es>
2022-10-09 19:00:13 +0200

    ejercicio 8c listo

--- ejercicio8b.cpp
+++ ejercicio8b.cpp
@@ -5,33 +5,25 @@ Date: 28/09/2022
 Goal: 
 ---------------------------------------------------*/
 #include <iostream>
-#include <iomanip>
-int main()
-{
-   const double nE = 2.71828182845904525356;
-    double e1 = 1.0, e2 = 1.0;
-    double margen = 0.00000000001;
-    double diferencia1 = nE - e1;
-    double diferencia2 = nE - e2;
-    double factorial = 1.0;
-    int k = 1;
-    int n = 1;
-    while (margen < diferencia1 || margen < diferencia2)
+#include <math.h>
+using namespace std;
 
-    {
-        e2 = e1;
-        for (int j = n; j <= k; j++)
+int main() {
+    float exponencial;
+    int i=0, n, numerador=1, denominador=1;
+    float e;
+    double factorial = 1;
 
-            factorial = factorial * j;
-        k++;
-        n++;
-        e1 += (1 / factorial);
-        diferencia1 = nE - e1;
-        diferencia2 = nE - e2;
+    cout << "Write the number of terms of the sum: ";
+    cin >> n;
 
+    while(i < n){
+        i++;
+        factorial *= i;
     }
-    std::cout << std::setprecision(20) << std::fixed;
-    std::cout << "\nValor de e = " << e1 << std::endl;
+    e = (numerador / denominador);
+    cout << "The value of e is: " << e << endl;
+
     return 0;
 
 }
--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -2,51 +2,35 @@
 File: ejercicio9.cpp
 Author: Toni Mari
 Date: 28/09/2022
-Goal: 
+Goal: This program calculates the number pi from an infinite series of operations.
+It shows a table of the first 1000 iterations.
 ---------------------------------------------------*/
 
 #include <iostream>
-
-#include <math.h>
-
 using namespace std;
 
-int main()
+int main() {
+  double numerador = 4;
+  double denominador = 1;
+  double pi;
+  int x = 0;
+  int op = 1;
+  int iteraciones = 1000;
+  int num = 0;
+  float pin = 3.14159;
+  while(x < iteraciones) {
+    num++;
+    x++;
+    pi += op * (numerador / denominador);
+    denominador += 2;
+    op *= -1;
+    
 
- 
+    cout << num << "\t" << pi << "\t" << pin << endl;
+  }
 
-{
-
-    double s =0;
-
- int n;
-
- int i;
-
- double a;
-
-cout<<"introdisca el valor de "<<endl;
-
-  cin>> n;
-
-    for (i=1;i<=n;i++)
-
-    {
-
-        a=(-4)*pow(-1,i)/(2*i-1);
-
- 
-
-        s= s + a;
-
-        }
-
-        cout <<"el  valor de  pi es :"<<s<<endl;
-
- 
-
-    system("pause");
-
-    return 0;
-
+  return 0;
 }
+
+
+  
